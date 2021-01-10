a545f02517e312d02c2d8528b5fa8e9b5d9898d0
BlackWingedKing <ritesh.goru@gmail.com>
2021-01-10 19:12:26 +0530

    base file structure

---
+++ test_parser.py
@@ -0,0 +1,30 @@
+"""
+    This code builds a small decision tree
+    trains over iris dataset
+
+    We test using the built decision tree
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+print(X_train.shape, X_test.shape)
+print(y_train.shape, y_test.shape)
+
+clf = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0)
+clf.fit(X_train, y_train)
+
+decision_tree = clf
+
+"""
+    Now the testing part
+"""
---
+++ tree_parser.py
@@ -0,0 +1,8 @@
+"""
+    This code parses a tree and returns a pytorch model
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
